59ea0950a9c53bef76893db88ab1306c4847d115
Lideyi1997 <448834752@qq.com>
2019-08-11 23:00:52 +0800

    添加图片上传

---
+++ src/main/java/life/lidy/community/controller/FileController.java
@@ -0,0 +1,43 @@
+package life.lidy.community.controller;
+
+import life.lidy.community.dto.FileDTO;
+import life.lidy.community.provider.UCloudProvider;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@Slf4j
+public class FileController {
+    @Autowired
+    private UCloudProvider uCloudProvider;
+
+    @RequestMapping("/file/upload")
+    @ResponseBody
+    public FileDTO upload(HttpServletRequest request) {
+        MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+        //获得图片文件
+        MultipartFile file = multipartRequest.getFile("editormd-image-file");
+        try {
+            //获取上传到服务器的文件路径
+            String fileName = uCloudProvider.upload(file.getInputStream(), file.getContentType(), file.getOriginalFilename());
+            FileDTO fileDTO = new FileDTO();
+            fileDTO.setSuccess(1);
+            //获取到上传路径后返回图片的urL 客户端通过分析json来解析图片url
+            fileDTO.setUrl(fileName);
+            return fileDTO;
+        } catch (Exception e) {
+            log.error("upload error", e);
+            FileDTO fileDTO = new FileDTO();
+            fileDTO.setSuccess(0);
+            fileDTO.setMessage("上传失败");
+            return fileDTO;
+        }
+    }
+}
---
+++ src/main/java/life/lidy/community/dto/FileDTO.java
@@ -0,0 +1,10 @@
+package life.lidy.community.dto;
+
+import lombok.Data;
+
+@Data
+public class FileDTO {
+    private int success;
+    private String message;
+    private String url;
+}
