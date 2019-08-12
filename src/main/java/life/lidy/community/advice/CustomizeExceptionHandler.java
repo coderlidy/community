250110264bd51c691aa7ddbac90d99a17c0ca028
Lideyi1997 <448834752@qq.com>
2019-08-12 23:08:39 +0800

    删除错误提示

--- src/main/java/life/lidy/community/advice/CustomizeExceptionHandler.java
+++ src/main/java/life/lidy/community/advice/CustomizeExceptionHandler.java
@@ -18,47 +18,47 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@ControllerAdvice
-@Slf4j
-public class CustomizeExceptionHandler {
-    @ExceptionHandler(Exception.class)
-    ModelAndView handle(Throwable e, Model model, HttpServletRequest request, HttpServletResponse response) {
-        String contentType=request.getContentType();
-        if ("application/json".equals(contentType)) {
-            // 返回 JSON
-            ResultDTO resultDTO;
-            if (e instanceof CustomizeException) {
-                resultDTO = ResultDTO.errorOf((CustomizeException) e);
-            } else {
-                log.error("handle error", e);
-                resultDTO = ResultDTO.errorOf(CustomizeErrorCode.SYS_ERROR);
-            }
-            try {
-                response.setContentType("application/json");
-                response.setStatus(200);
-                response.setCharacterEncoding("UTF-8");
-                PrintWriter writer=response.getWriter();
-                writer.write(JSON.toJSONString(resultDTO));
-                writer.close();
-            } catch (IOException ex) {
-            }
-            return null;
-        }else{
-            //错误页面
-            if (e instanceof CustomizeException) {
-                model.addAttribute("message",e.getMessage());
-            } else {
-                log.error("handle error", e);
-                model.addAttribute("message",CustomizeErrorCode.SYS_ERROR.getMessage());
-            }
-            return new ModelAndView("error");
-        }
-    }
-    private HttpStatus getStatus(HttpServletRequest request) {
-        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
-        if (statusCode == null) {
-            return HttpStatus.INTERNAL_SERVER_ERROR;
-        }
-        return HttpStatus.valueOf(statusCode);
-    }
-}
+//@ControllerAdvice
+//@Slf4j
+//public class CustomizeExceptionHandler {
+//    @ExceptionHandler(Exception.class)
+//    ModelAndView handle(Throwable e, Model model, HttpServletRequest request, HttpServletResponse response) {
+//        String contentType=request.getContentType();
+//        if ("application/json".equals(contentType)) {
+//            // 返回 JSON
+//            ResultDTO resultDTO;
+//            if (e instanceof CustomizeException) {
+//                resultDTO = ResultDTO.errorOf((CustomizeException) e);
+//            } else {
+//                log.error("handle error", e);
+//                resultDTO = ResultDTO.errorOf(CustomizeErrorCode.SYS_ERROR);
+//            }
+//            try {
+//                response.setContentType("application/json");
+//                response.setStatus(200);
+//                response.setCharacterEncoding("UTF-8");
+//                PrintWriter writer=response.getWriter();
+//                writer.write(JSON.toJSONString(resultDTO));
+//                writer.close();
+//            } catch (IOException ex) {
+//            }
+//            return null;
+//        }else{
+//            //错误页面
+//            if (e instanceof CustomizeException) {
+//                model.addAttribute("message",e.getMessage());
+//            } else {
+//                log.error("handle error", e);
+//                model.addAttribute("message",CustomizeErrorCode.SYS_ERROR.getMessage());
+//            }
+//            return new ModelAndView("error");
+//        }
+//    }
+//    private HttpStatus getStatus(HttpServletRequest request) {
+//        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+//        if (statusCode == null) {
+//            return HttpStatus.INTERNAL_SERVER_ERROR;
+//        }
+//        return HttpStatus.valueOf(statusCode);
+//    }
+//}
